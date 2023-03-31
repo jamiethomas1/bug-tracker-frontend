6d4a5fdfc15668a4cf7f76466f69e78846557f78
cogsincogs <jamiewthomas13@gmail.com>
2023-03-31 20:12:39 +0100

    Update index page title to Bug Tracker

--- components/layout.jsx
+++ components/layout.jsx
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Jamie Thomas';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Bug Tracker';
 
 export default function Layout({ children, home }) {
     return (
