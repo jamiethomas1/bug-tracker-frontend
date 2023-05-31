c72e8292e30c418e109e7a8530f840550cb38bff
cogsincogs <jamiewthomas13@gmail.com>
2023-05-31 16:02:31 +0100

    Add SessionProvider to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 import { useEffect } from 'react';
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ 
+  Component, 
+  pageProps: { session, ...pageProps }
+}) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
