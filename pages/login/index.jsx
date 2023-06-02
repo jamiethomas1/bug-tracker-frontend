7f320185478de9be9ca87bb70789876b71c048ae
cogsincogs <jamiewthomas13@gmail.com>
2023-06-02 22:07:07 +0100

    Replace form with a single SignIn component

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,23 +1,10 @@
+import SignIn from "../../components/signin";
+
 export default function LoginPage() {
     return (
         <>
             <h1>Login</h1>
-
-            <form method="post" noValidate>
-            <div>
-                <input type="email" name="email" id="email" placeholder="Email"
-                        defaultValue="EMAIL" />
-            </div>
-            <div>
-                <input type="password" name="password" id="password" placeholder="Password" />
-            </div>
-            <div>
-                <p>New here? <a href="/sign-up">Create an account</a>.</p>
-            </div>
-            <div>
-                <input type="submit" defaultValue="Log in" />
-            </div>
-        </form>
+            <SignIn />
         </>
     );
 }
