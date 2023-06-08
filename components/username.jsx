4a0dbddbd85867ce05d610d88a0c28361c615a84
cogsincogs <jamiewthomas13@gmail.com>
2023-06-08 15:57:36 +0100

    Add error handling

--- components/username.jsx
+++ components/username.jsx
@@ -1,11 +1,14 @@
 import utilStyles from '../styles/utils.module.css';
 
 export default function Usernames({ users }) {
-    return (
-        <ul>
-            {users.map(({name}) => (
-                <li className={utilStyles.listItem} key={name}>{name}</li>
-            ))}
-        </ul>    
-    );
+    if (users){
+        return (
+            <ul>
+                {users.map(({name}) => (
+                    <li className={utilStyles.listItem} key={name}>{name}</li>
+                ))}
+            </ul>    
+        );
+    }
+    return null;
 }
