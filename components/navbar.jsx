4d66915ac7e4071f09081087b30cbe303ba99fc3
cogsincogs <jamiewthomas13@gmail.com>
2023-06-12 13:05:37 +0100

    Use profile image provided by login provider

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -30,10 +30,7 @@ export default function Navbar({ session }) {
                         </ul>
                         <div className="dropdown">
                             <button className="btn btn-secondary dropdown-toggle d-flex align-items-center gap-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                                    <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                                </svg>
+                                <img src={session.user.image} alt="Profile picture" className={styles.profilepicture}/>
                                 <p className="text-white mb-0">{session.user.name}</p>
                             </button>
                             <ul className="dropdown-menu">
