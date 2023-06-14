b1d2e3f5ceeb8dc7b7ceb042f2ee8308145882f1
cogsincogs <jamiewthomas13@gmail.com>
2023-06-14 10:54:42 +0100

    Persist session to My Organisations page

--- pages/my/organisations/index.jsx
+++ pages/my/organisations/index.jsx
@@ -1,7 +1,22 @@
+import Layout from "../../../components/layout"
+import { useSession } from "next-auth/react"
+
 export default function Organisations() {
+    const { data: session } = useSession();
+
+    if (!session) {
+        return (
+            <>
+              <a href='/api/auth/signin'>Sign in</a>
+            </>
+          )
+    }
+
     return (
         <>
-        <p>Hello</p>
+        <Layout session={session}>
+            <p>Hello</p>
+        </Layout>
         </>
     )
 }
